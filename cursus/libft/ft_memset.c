35b9ecdeca081b75e899782ff12cb3c467f19941
ameena2394 <ameenamustafa1@gmail.com>
2022-09-25 15:28:41 +0400

    25sep

--- cursus/libft/ft_memset.c
+++ cursus/libft/ft_memset.c
@@ -10,4 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*string;
 
+	string = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		string[i] = c;
+		i++;
+	}
+	return (s);
+}
