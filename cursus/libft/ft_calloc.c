1142f4fd72a7ed870acd1a30a8429c2b02f9c694
ameena2394 <ameenamustafa1@gmail.com>
2022-09-28 13:30:32 +0400

    st

--- cursus/libft/ft_calloc.c
+++ cursus/libft/ft_calloc.c
@@ -10,4 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void	*ft_calloc(size_t number, size_t size)
+{
+	void *array;
 
+	array = (void *)malloc(size * number);
+	if (!array)
+		return (NULL);
+	ft_bzero(array, size * number);
+	return ((void *)array);
+}
--- cursus/libft/ft_strchr.c
+++ cursus/libft/ft_strchr.c
@@ -10,4 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strchr(const char *s, int c)
+{
+	char	*str;
 
+	str = (char *)s;
+	while (*str != c)
+	{
+		if (*str == '\0')
+		{
+			return (NULL);
+		}
+		str++;
+	}
+	return (str);
+}
