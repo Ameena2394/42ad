0af60cd65d10361fc0ef2204119c611856d27940
ameena2394 <ameenamustafa1@gmail.com>
2022-10-24 21:16:33 +0400

    striteri

--- cursus/libft/ft_striteri.c
+++ cursus/libft/ft_striteri.c
@@ -13,19 +13,15 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	int		i;
-	int		i2;
+	
+	int	i;
 
 	i = 0;
-	i2 = 0;
-	if (s != NULL && f != NULL)
+	if (!s)
+		return ;
+	while (s[i] != '\0')
 	{
-		i = ft_strlen(s);
-		while (i2 < i)
-		{
-			(*f)(i2, s);
-			s++;
-			i2++;
-		}
+		f(i, &s[i]);
+		i++;
 	}
 }
