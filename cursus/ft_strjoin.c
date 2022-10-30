8c1d2ef55ce42cee0549478192fe2cd6982c075e
ameena2394 <ameenamustafa1@gmail.com>
2022-10-30 17:24:14 +0400

    norm

--- cursus/ft_strjoin.c
+++ cursus/ft_strjoin.c
@@ -11,20 +11,20 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-char    *ft_strjoin(char const *s1, char const *s2)
-  {
-      char    *str;                                                                         
-      int     s1_len;
-      int     s2_len;
-      
-      if (!s1 || !s2)            
-      return (NULL);
-      s1_len = ft_strlen(s1);      
-      s2_len = ft_strlen(s2);
-      if (!(str = (char*)malloc(sizeof(char) * (s1_len + s2_len + 1)))) 
-          return (NULL);
-      ft_memcpy(str, s1, s1_len);        
-      ft_memcpy(str + s1_len, s2, s2_len); 
-      str[s1_len + s2_len] = '\0';    
-      return (str);
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		s1_len;
+	int		s2_len;
+	
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	if (!(str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1))))
+		return (NULL);
+	ft_memcpy (str, s1, s1_len);
+	ft_memcpy (str + s1_len, s2, s2_len);
+	str [s1_len + s2_len] = '\0';
+	return (str);
 }
